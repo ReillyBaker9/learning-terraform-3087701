4060afff7e93ee6f39fdcde0cfb1bacec841e8f5
ReillyBaker9 <67935807+ReillyBaker9@users.noreply.github.com>
2026-06-05 09:09:54 -0400

    Add variable for EC2 instance type and remove old data
    Added a variable for instance type to allow customization of EC2 instance type, while removing the previous data source for free-tier instance types.

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "instance_type" {
+  description = "EC2 instance type - must be free-tier eligible in your region (t3.micro or t2.micro)"
+  default     = "t3.micro"
+}
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,21 +19,9 @@ data "aws_ami" "app_ami" {
   owners = ["amazon"]
 }
 
-data "aws_ec2_instance_type_offerings" "free_tier" {
-  filter {
-    name   = "free-tier-eligible"
-    values = ["true"]
-  }
-
-  filter {
-    name   = "instance-type"
-    values = ["t3.micro", "t2.micro", "t4g.micro"]
-  }
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = data.aws_ec2_instance_type_offerings.free_tier.instance_types[0]
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
